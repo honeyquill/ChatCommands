213653465c534919f29a711803a910b988cff011
Bee <beehoneyquill@gmail.com>
2026-04-07 03:20:40 -0600

    Updating to Quill Standard

--- Main.cs
+++ Main.cs
@@ -16,48 +16,31 @@ namespace ChatCommands
     {
         private string _newestMsg;
         private const char CommandPrefix = '!';
+        
         private readonly Dictionary<string, ChatCommand> _commands =
             new Dictionary<string, ChatCommand>();
         
-        public IReadOnlyDictionary<string, ChatCommand> Commands => _commands;
-        
+        public void RegisterCommand(string command, ChatCommand chatCommand)
+        {
+            _commands.Add(command.ToLower(), chatCommand);
+            MelonLogger.Msg("Command " + chatCommand.Name + " registered!");
+        }
+
         public override void OnUpdate()
         {
             CommandHandler();
         }
 
-        public void RegisterCommand(string command, ChatCommand chatCommand)
-        {
-            _commands.Add(command.ToLower(), chatCommand); 
-            MelonLogger.Msg("Command " + chatCommand.Name + " registered!");
-        }
-
-        private string GetNewestCommand()
-        {
-            var history = GetChatHistory();
-
-            if (history == null || history.Count == 0)
-            {
-                return null;
-            }
-                
-            
-            var newestIndex = history.Count - 1;
-            var lastMessage = _newestMsg;
-            _newestMsg = history[newestIndex].message?.ToLower();
-            Debug.Assert(_newestMsg != null, nameof(_newestMsg) + " != null");
-            
-            if (_newestMsg == lastMessage) return null;
-            if (_newestMsg.ToCharArray()[0] != CommandPrefix) return null;
-            
-            _newestMsg = _newestMsg.Remove(0, 1);
-            MelonLogger.Msg("Command identified:" + _newestMsg);
-            return _newestMsg;
-        }
-
         private void CommandHandler()
         {
-            var commandName = GetNewestCommand();
+            foreach (var command in _commands.Select(kvp => kvp.Value))
+            {
+                command.Update();
+            }
+            var result = GetNewestCommand();
+            if (result == null) return;
+            var (commandName, args, player) = result.Value;
+
             switch (commandName)
             {
                 case null:
@@ -77,12 +60,42 @@ namespace ChatCommands
 
             if (_commands.TryGetValue(commandName, out var commandAction))
             {
-                commandAction.Execute(Array.Empty<string>());
+                commandAction.Execute(args,player);
             }
             else
             {
                 SendChatMessage("Unknown command: " + commandName);
             }
+        }
+        
+        private (string command, string[] args,string player)? GetNewestCommand()
+        {
+            var history = GetChatHistory();
+            if (history == null || history.Count == 0)
+                return null;
+
+            var newestIndex = history.Count - 1;
+            var lastMessage = _newestMsg;
+            _newestMsg = history[newestIndex].message?.ToLower();
+            Debug.Assert(_newestMsg != null, nameof(_newestMsg) + " != null");
+
+            if (_newestMsg == lastMessage) return null;
+            if (_newestMsg.ToCharArray()[0] != CommandPrefix) return null;
+
+            var withoutPrefix = _newestMsg.Substring(1);
+
+            var parts = withoutPrefix
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var commandName = parts[0];
+            var args = parts.Length > 1
+                ? parts.Skip(1).ToArray()
+                : Array.Empty<string>();
+            var player = history[newestIndex].player;
+
+            MelonLogger.Msg("Command identified:" + _newestMsg);
+
+            return (commandName, args,player);
         }
     }
 }
